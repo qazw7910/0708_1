491d6cc627d6ec27c6dd95c2ced84545c77d7823
qazw7910 <okmijn7980@gmail.com>
2021-07-08 09:33:43 +0800

    test

---
+++ src/settings.14c2b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Player_Start.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",resources:"1b2f5",main:"45e39"}};
